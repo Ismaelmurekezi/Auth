0071404362f6b2be62a25bc2658be2cd89c11d82
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2024-08-09 10:33:16 +0200

    adding content to home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <div>
+      <p className="w-[600px] m-auto py-96 text-2xl font-sans">
+        This project is a fully functional authentication system built using the
+        MERN stack (MongoDB, Express.js, React.js, and Node.js). It allows users
+        to securely log in to the application using their Google account,
+        leveraging OAuth 2.0 for authentication.
+      </p>
+    </div>
+  );
 }
 
 export default About
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div className="relative h-screen w-full">
+      <img
+        src="/background Image.jpg"
+        alt="Background"
+        className="absolute h-full w-full object-cover"
+      />
+      <div className="relative flex items-center justify-center h-full">
+        <p className="w-[800px] bg-transparent text-white bg-opacity-75 text-center p-8 rounded-lg text-2xl font-sans">
+          This project is a fully functional authentication system built using
+          the MERN stack (MongoDB, Express.js, React.js, and Node.js). It allows
+          users to securely log in to the application using their Google
+          account, leveraging OAuth 2.0 for authentication.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home
